8592a627eca5aaf720f7dc0bd6ee0021a34ccd5a
sapho-sys <saphonkunzi@gmail.com>
2022-08-30 12:48:05 +0200

    yeah

--- data-factory.js
+++ data-factory.js
@@ -1,40 +1,39 @@
-function theWaiters(db){
+function theWaiters(db) {
     const data = db;
     let errorMsg = '';
-    
-	const RegExp = /^[A-Za-z]+$/;
 
-    async function setEmployee(user){
+    const RegExp = /^[A-Za-z]+$/;
+
+    async function setEmployee(user) {
         let waitername = user.trim()
         try {
-            if(waitername !== ''){
-                if(waitername.match(RegExp)){
+            if (waitername !== '') {
+                if (waitername.match(RegExp)) {
                     let waiter = waitername.charAt(0).toUpperCase + waitername.slice(1).toLowerCase();
-                    const sqlDuplicates = await data.manyOrNone('SELECT waiter_name FROM my_waiters WHERE waiter_name = $1',[waiter]);
-                    if(sqlDuplicates.length == 0){
-                        await data.manyOrNone('INSERT INTO my_waiters (waiter_name) VALUES ($1)',[waiter]);
-
-                    }else{
+                    const sqlDuplicates = await data.manyOrNone('SELECT waiter_name FROM my_waiters WHERE waiter_name = $1', [waiter]);
+                    if (sqlDuplicates.length == 0) {
+                        await data.manyOrNone('INSERT INTO my_waiters (waiter_name) VALUES ($1)', [waiter]);
+                    } else {
                         errorMsg = 'Note: This waiter has already done their schedule for the week!'
                     }
-                }else{
+                } else {
                     errorMsg = 'Invalid characters entered!, please try again...'
                 }
-            } else{
+            } else {
                 errorMsg = 'Please provide your name !'
             }
-            
+
         } catch (error) {
             console.error('Somethin went wrong', err);
-			throw err;
+            throw err;
         }
 
     }
-    function errors(){
+    function errors() {
         return errorMsg;
     }
 
-    async function retrieveData(){
+    async function retrieveData() {
         const tableRow = data.manyOrNone('SELECT waiter_name FROM my_waiters');
         return tableRow;
     }
